b8de65c88dab3a9ba1284bd0a7317ad031762072
Luke Schumaker <luke@Lukes-MacBook-Air.local>
2023-06-29 11:32:07 -0700

    homework update

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,1 +1,11 @@
-git addgit
+import os
+import csv
+
+csvpath = os.path.join('python-challenge', 'PyBank/Resources', 'budget_data.csv')
+
+total_months = 0
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
